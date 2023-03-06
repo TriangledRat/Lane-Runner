b66361bdd901ecc25350f5e041e5b3827189f489
Michael Smidt <michael.smidt@hva.nl>
2023-03-06 15:34:38 +0100

    Added game over screen

---
+++ Lane Runner/Assets/Scripts/GameOverManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverManager : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+        
+    }
+}
